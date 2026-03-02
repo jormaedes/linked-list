76dd49459cc19471a511fe5df34218e6faaefcc2
Jormaedes Luis <jormahed@gmail.com>
2026-03-02 16:43:07 +0100

    fix(LinkedList.js): inicializei os valores com undefined

--- LinkedList.js
+++ LinkedList.js
@@ -2,8 +2,8 @@ import Node from "./Node";
 
 class LinkedList {
 	constructor() {
-		this._head = null;
-		this._tail = null;
+		this._head = undefined;
+		this._tail = undefined;
 		this._size = 0;
 	}
 
